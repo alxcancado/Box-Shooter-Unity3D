698cea523be3e258ef834479bf0f294660071f00
alxcancado <alxcancado@gmail.com>
2015-10-19 16:58:43 +1300

    First Commit. Project is halfway

---
+++ Assets/Scripts/BasicTragetMover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTragetMover : MonoBehaviour {
+
+	public float spinSpeed = 180.0f;
+	public float motionMagnitude = 0.1f;
+
+	public bool doSpin = true;
+	public bool doMotion = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (doSpin){
+
+			// rotate around the up axis of the gameObject. Vector3.up usually is (0,1,0)
+			gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+
+		}
+
+		if (doMotion) {
+
+			// move up and down over time
+			gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+
+		}
+
+	
+	}
+}
